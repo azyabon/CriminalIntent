dbd9b75c70f10c4d5aa1d348e7ede9549eb2151b
Andrey Azyabin <andrey.azyabin@bk.ru>
2022-11-06 14:57:59 +0700

    date_fragment

--- app/src/main/java/com/example/criminalintent/IntentFragment.kt
+++ app/src/main/java/com/example/criminalintent/IntentFragment.kt
@@ -1,12 +1,98 @@
 package com.example.criminalintent
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import com.example.criminalintent.databinding.FragmentIntentBinding
+import com.google.android.material.textfield.TextInputEditText
+import java.util.*
 
-class IntentFragment : Fragment() {
+class IntentFragment : Fragment(R.layout.fragment_intent) {
+
+    private val calendar = Calendar.getInstance()
+    private var _binding: FragmentIntentBinding? = null
+    private val binding get() = _binding!!
+
+    private var tvDate: String = calendar.get(Calendar.DAY_OF_MONTH).toString() +
+            "-" + calendar.get(Calendar.MONTH).toString() +
+            "-" + calendar.get(Calendar.YEAR).toString()
+    private var isSolved: Boolean = false
+    private lateinit var img: Bitmap;
+    private lateinit var imgView: ImageView;
+    private var text: String = ""
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentIntentBinding.bind(view)
+
+
+        val dateInput = view.findViewById(R.id.dateInput) as TextView
+        val solvCheck = view.findViewById(R.id.solvCheck) as CheckBox
+        val photoBtn = view.findViewById(R.id.photoBtn) as Button
+        val titleInput = view.findViewById(R.id.titleInput) as TextInputEditText
+        imgView = view.findViewById(R.id.imgView) as ImageView
+
+        dateInput.text = tvDate.toString()
+        solvCheck.isChecked = isSolved
+
+        photoBtn.setOnClickListener {
+            var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, 123)
+        }
+
+        solvCheck.setOnClickListener {
+            isSolved = !isSolved
+        }
+
+        titleInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                text = titleInput.text.toString()
+                println(text)
+            }
+        })
+
+        binding.apply {
+            dateInput.setOnClickListener {
+                val datePickerFragment = DatePickerFragment()
+                val supportFragmentManager = requireActivity().supportFragmentManager
+                datePickerFragment.show(parentFragmentManager, "")
+                supportFragmentManager.setFragmentResultListener(
+                    "REQUEST_KEY",
+                    viewLifecycleOwner
+                    ) { resultKey, bundle ->
+                    if (resultKey == "REQUEST_KEY") {
+                        val date = bundle.getString("SELECTED_DATE")
+                        tvDate = date.toString()
+                        dateInput.text = date
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 123) {
+            var bmp = data?.extras?.get("data") as Bitmap
+            img = bmp
+            imgView.setImageBitmap(bmp)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/example/criminalintent/MainActivity.kt
+++ app/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.criminalintent
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.criminalintent.databinding.ActivityMainBinding
@@ -10,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private val adapter = IntentListAdapter()
     private val imgIdList = listOf(R.drawable.black_box, R.drawable.black_box)
     private var index = 0
+    private var fragment =  IntentFragment.newInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,19 +21,36 @@ class MainActivity : AppCompatActivity() {
         init()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(false)
+            supportFragmentManager.beginTransaction().remove(fragment).commit()
+        } else if (item.itemId == R.id.add) {
+            supportFragmentManager.beginTransaction().replace(R.id.intentForm, fragment).commit()
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            if (index < imgIdList.size - 1) {
+                var intent = IntentModel("Kill human", "10 Jan 2022", imgIdList[index])
+                adapter.addIntent(intent)
+                index++
+            } else {
+                index = 0
+            }
+        }
+
+        return true
+    }
+
     private fun init() {
         binding.apply {
             rcView.layoutManager = GridLayoutManager(this@MainActivity, 1)
             rcView.adapter = adapter
-            buttonAdd.setOnClickListener {
-                if (index < imgIdList.size - 1) {
-                    var intent = IntentModel("Kill human", "10 Jan 2022", imgIdList[index])
-                    adapter.addIntent(intent)
-                    index++
-                } else {
-                    index = 0
-                }
-            }
+                // выводить все что сохранено в бд при инициализации
         }
     }
 }
